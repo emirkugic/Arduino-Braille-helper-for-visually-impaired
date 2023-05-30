223d883eda43f01afc458b0f9faabb2c673a2357
Emir Kugić <103369144+emirkugic@users.noreply.github.com>
2023-05-30 17:52:40 +0200

    less code less bugs

--- arduino code.cpp
+++ arduino code.cpp
@@ -1,28 +1,16 @@
 #include <Servo.h>
 
 //connection pins
-const int servo1Pin = 1;
-const int servo2Pin = 2;
-const int servo3Pin = 3;
-const int servo4Pin = 4;
-const int servo5Pin = 5;
-const int servo6Pin = 6;
+const int servoPins[] = {1, 2, 3, 4, 5, 6};
+const int servoCount = sizeof(servoPins) / sizeof(servoPins[0]);
 
 //switch pin
-int switchPin = 11;
-
-
+const int switchPin = 11;
 
 //settings constants and instances for electronics, etc
 const int pauseTime = 250;
-Servo servo1, servo2, servo3, servo4, servo5, servo6;
-const int servoCount = 6;
-Servo servos[servoCount] = {servo1, servo2, servo3, servo4, servo5, servo6};
-int servoPins[servoCount] = {servo1Pin, servo2Pin, servo3Pin, servo4Pin, servo5Pin, servo6Pin};
+Servo servos[servoCount];
 int servoOriginalPositions[servoCount];
-//delay between letters
-const int letterDelay = pauseTime;
-
 
 //setup function
 void setup() {
@@ -33,20 +21,12 @@ void setup() {
   pinMode(switchPin, INPUT_PULLUP);
 }
 
-
-
-
 //main function
 void loop(){
   powerSwitch();
 }
 
-
-
-
 bool deviceOn = false;
-bool shouldStop = false;
-bool switchState = false;
 
 //function for the on and off switch
 void powerSwitch() {
@@ -56,42 +36,32 @@ void powerSwitch() {
     // Turn on the device
     if (switchState == LOW && !deviceOn) {
       deviceOn = true;
-      shouldStop = false;
       runDevice();
     }
     // Turn off the device
     else if (switchState == HIGH && deviceOn) {
       deviceOn = false;
-      shouldStop = true;
       resetServos();
     }
   }
 }
 
-
-
-
 // runs the device
 void runDevice() {
   String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  for (int i = 0; i < 26; i++) {
+  for (int i = 0; i < letters.length(); i++) {
     executeLetter(letters[i]);
     resetServos();
     delay(pauseTime);
     if (digitalRead(switchPin) == HIGH) {
       break;
     }
-    if (i>26){
-      break;
-    }
   }
 }
 
-
-
-
 const int servoIndices[26][6] = {
+  // Your servo indices...
   {0, -1, -1, -1, -1, -1}, // A
   {0, -1, 2, -1, -1, -1}, // B
   {0, 1, -1, -1, -1, -1}, // C
@@ -125,204 +95,11 @@ void executeLetter(char letter) {
   for (int i = 0; i < servoCount; i++) {
     int servoIndex = servoIndicesForLetter[i];
     if (servoIndex >= 0) {
-      int servoPosition = getServoPositionForLetter(letter, i);
-      servos[servoIndex].write(servoPosition);
+      servos[servoIndex].write(0);
     }
   }
-  delay(letterDelay);
+  delay(pauseTime);
 }
-
-
-//this function tells the arduino which servo to move for which letter
-int getServoPositionForLetter(char letter, int servoIndex) {
-    switch (letter) {
-      
-        case 'A':
-            if (servoIndex == 0) {
-                return 0;
-            }
-            break;
-
-
-        case 'B':
-            if (servoIndex == 0 || servoIndex == 2) {
-                return 0;
-            }
-            break;
-
-
-        case 'C':
-            if (servoIndex == 0 || servoIndex == 1) {
-                return 0;
-            }
-            break;
-
-
-        case 'D':
-            if (servoIndex == 0 || servoIndex == 1 || servoIndex == 3) {
-                return 0;
-            }
-            break;
-
-
-        case 'E':
-            if (servoIndex == 0 || servoIndex == 3) {
-                return 0;
-            }
-            break;
-
-
-        case 'F':
-            if (servoIndex == 0 || servoIndex == 1 || servoIndex == 2) {
-                return 0;
-            }
-            break;
-        case 'G':
-            if (servoIndex == 0 || servoIndex == 1 || servoIndex == 2 || servoIndex == 3) {
-                return 0;
-            }
-            break;
-
-
-        case 'H':
-          if (servoIndex == 0 || servoIndex == 1 || servoIndex == 2 || servoIndex == 3) {
-            return 0;
-          }
-          break;
-
-
-        case 'I':
-            if (servoIndex == 1 || servoIndex == 2) {
-                return 0;
-            }
-            break;
-
-
-        case 'J':
-            if (servoIndex == 1 || servoIndex == 2 || servoIndex == 3) {
-                return 0;
-            }
-            break; 
-
-
-        case 'K':
-            if (servoIndex == 0 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-            
-
-        case 'L':
-            if (servoIndex == 0 || servoIndex == 2 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-
-        case 'M':
-            if (servoIndex == 0 || servoIndex == 1 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-
-        case 'N':
-            if (servoIndex == 0 || servoIndex == 1 || servoIndex == 3 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-
-        case 'O':
-            if (servoIndex == 0 || servoIndex == 3 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-
-        case 'P':
-            if (servoIndex == 0 || servoIndex == 1 || servoIndex == 2 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-
-        case 'Q':
-            if (servoIndex == 0 || servoIndex == 1 || servoIndex == 2 || servoIndex == 3 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-
-        case 'R':
-            if (servoIndex == 0 || servoIndex == 2 || servoIndex == 3 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-
-        case 'S':
-            if (servoIndex == 1 || servoIndex == 2 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-
-        case 'T':
-            if (servoIndex == 1 || servoIndex == 2 || servoIndex == 3 || servoIndex == 4) {
-                return 0;
-            }
-            break;
-            
-
-        case 'U':
-            if (servoIndex == 0 || servoIndex == 4 || servoIndex == 5) {
-              return 0;
-            }
-            break;
-
-
-        case 'V':
-          if (servoIndex == 0 || servoIndex == 2 || servoIndex == 4 || servoIndex == 5) {
-            return 0;
-          }
-          break;
-
-
-        case 'W':
-          if (servoIndex == 1 || servoIndex == 2 || servoIndex == 3 || servoIndex == 4 || servoIndex == 5) {
-            return 0;
-          }
-          break;
-
-
-        case 'X':
-          if (servoIndex == 0 || servoIndex == 1 || servoIndex == 4 || servoIndex == 5) {
-            return 0;
-          }
-          break;
-
-
-        case 'Y':
-          if (servoIndex == 0 || servoIndex == 1 || servoIndex == 3 || servoIndex == 4 || servoIndex == 5) {
-            return 0;
-          }
-          break;
-
-
-        case 'Z':
-          if (servoIndex == 0 || servoIndex == 3 || servoIndex == 4 || servoIndex == 5) {
-            return 0;
-          }
-          break;
-
-
-    }
-
-    // return original position if no position was returned earlier
-    return servoOriginalPositions[servoIndex];
-}
-
 
 // reset servos to 90 degree angle (default position)
 void resetServos() {
@@ -330,6 +107,3 @@ void resetServos() {
     servos[i].write(90); 
   }
 }
-
-
-
